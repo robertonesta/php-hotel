4fa7cb98ad7830f76283a38e8786ad8614c45ef3
RobertoNesta <robertonesta1993@gmail.com>
2023-05-04 15:10:49 +0200

    creation of the objects array

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+$hotels= [
+    [
+        "name" => "Burj Al Arab Hotel",
+        "parkingLot"  => "true",
+        "vote"  => 5,
+    ],
+    [
+        "name" => "Plaza Hotel",
+        "parkingLot"  => "true",
+        "vote"  => 5,
+    ],
+    [
+        "name" => "Albergo Hotel De Rossi",
+        "parkingLot"  => "false",
+        "vote"  => 2,
+    ],
+    [
+        "name" => "Hotel Kensington",
+        "parkingLot"  => "true",
+        "vote"  => 4,
+    ],
+    [
+        "name" => "Hotel Milano Ornato",
+        "parkingLot"  => "false",
+        "vote"  => 3,
+    ],
+]
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Hotel</title>
+</head>
+
+<body>
+
+<!-- Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate per steps:
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+Bonus: 1
+Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.-->
+
+
+
+
+
+</body>
+</html>
